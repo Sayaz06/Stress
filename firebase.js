f5ff41725fc0a1aaeb2a353d5b72604ca1708596
Sayaz06 <adha86614@gmail.com>
2025-11-27 23:53:49 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeaaPQnLkSlUshc735_CwUH35gxLRsdbY",
+  authDomain: "mytimingplanner-8bccb.firebaseapp.com",
+  projectId: "mytimingplanner-8bccb",
+  storageBucket: "mytimingplanner-8bccb.firebasestorage.app",
+  messagingSenderId: "925754719118",
+  appId: "1:925754719118:web:0ff929defcd80ca15c82df",
+  measurementId: "G-X3TVBZMW7W"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
