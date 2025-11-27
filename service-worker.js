6a1bd48aea0444bd0f5da2dffd841800fd2149b7
Sayaz06 <adha86614@gmail.com>
2025-11-27 23:55:34 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE = "stress-cache-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./main.js",
+  "./firebase.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
