264bd5138cd1b6466005dd6f68d380e091aabdce
Sayaz06 <adha86614@gmail.com>
2025-11-28 13:59:32 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 /* ============================================================
-   firebase.js — FINAL VERSION
-   (Guna config sebenar yang ad bagi)
+   firebase.js — FINAL VERSION (Project Baru)
 ============================================================ */
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
@@ -9,13 +8,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebas
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAeaaPQnLkSlUshc735_CwUH35gxLRsdbY",
-  authDomain: "mytimingplanner-8bccb.firebaseapp.com",
-  projectId: "mytimingplanner-8bccb",
-  storageBucket: "mytimingplanner-8bccb.firebasestorage.app",
-  messagingSenderId: "925754719118",
-  appId: "1:925754719118:web:0ff929defcd80ca15c82df",
-  measurementId: "G-X3TVBZMW7W"
+  apiKey: "AIzaSyCo41_eKC_s_ZNjaTASyMsByQgVuM_MVAs",
+  authDomain: "stress-timing-planner-prod.firebaseapp.com",
+  projectId: "stress-timing-planner-prod",
+  storageBucket: "stress-timing-planner-prod.firebasestorage.app",
+  messagingSenderId: "833808167996",
+  appId: "1:833808167996:web:0f1653572e1cacd7b23f39",
+  measurementId: "G-H16W0ZSVNT"
 };
 
 // ✅ Initialize Firebase
